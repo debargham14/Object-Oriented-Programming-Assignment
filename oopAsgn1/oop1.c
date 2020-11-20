da89854906466fd6985a4b53a389e716fc60e54b
debargham14 <debargham14@gmail.com>
2020-11-20 22:09:10 +0530

    minor changes

--- oopAsgn1/oop1.c
+++ oopAsgn1/oop1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    int a = 10;
-    int *p = &a;
-    int *pp = p;
-    
-    printf("%d %d %d", a, *p, *pp);
-    return 0;
+	int a = 10;
+	int *p = &a;
+	int **pp = &p;
+
+	printf("%d %d %d", a, *p, **pp);
+	return 0;
 }
