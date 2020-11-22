33195af0c2cb5e3d17a7411b5c8aac8986f4c6cc
debargham14 <debargham14@gmail.com>
2020-11-23 03:01:22 +0530

    new probs added

--- oopsAsgn3/prob11.cpp
+++ oopsAsgn3/prob11.cpp
@@ -2,68 +2,50 @@
 #define MAX_SIZE 100
 using namespace std;
 
-//defining the class applicant
+//designing the class applicant to perform the relevant operation
 class APPLICANT {
-	static int lastId;
+	static int lastId; //static variable declared to implement the auto increment feature
 	int id;
-	char name[30];
+	char name[20];
 	int score;
 
 public:
+	//method to recieve applicant data
+	void receiveData() {
+		cout << "Enter the name of the applicant :- ";
+		cin >> name;
+		cout << "Enter the score of the student :- ";
+		cin >> score;
+		id = ++lastId; //auto increment feature enbaled using static data member
+	}
 
-	void receiveData();
-	void showData();
-	static void showCount();
+	//show applicant data
+	void showData() {
+		cout << "Id of the student :- " << id << "\n";
+		cout << "Name of the student :- " << name << "\n";
+		cout << "Score of the student :- " << score << "\n";
+	}
+
+	//use of static data member is made to make it independent of the instance and can be called wihtout the use of object
+	static void totalCount() {
+		cout << "Total number of students registered so far :- " << lastId << "\n";
+	}
 };
 
-//function to receive the data
-void APPLICANT::receiveData() {
-	id = ++lastId;
-	cout << "Enter the name :- ";
-	cin >> name;
-
-	cout << "Enter the score :- ";
-	cin >> score;
-}
-
-//function to show the data
-void APPLICANT::showData() {
-	cout << "Appliaction Id" << id << "\n";
-	cout << "Name of the student :- " << name << "\n";
-	cout << "Score of the applicant :- " << score << "\n";
-}
-
-//function to show the applicant count
-void APPLICANT::showCount() {
-	cout << "The number of registered applicants = " << lastId << "\n";
-}
-
-
+int APPLICANT::lastId = 0;
 
 int main() {
+	//defining instances to use the methods
+	APPLICANT a1, a2;
 
-	cout << "Enter 1 to enter applicant data and 0 to stop entering data";
-	int ch, i = 0;
+	a1.receiveData();
+	a2.receiveData();
 
-	APPLICANT a[MAX_SIZE];
-	while (1) {
-		cin >> ch;
-		if (!ch) {
-			break;
-		}
-		else {
-			a[i].receiveData();
-			i++;
-		}
-	}
+	a1.showData();
+	a2.showData();
 
-	APPLICANT::showCount();
-
-	for (int j = 0; j < i; j++) {
-		cout << "Details of applicant " << i + 1 << "--------\n";
-		a[i].showData();
-	}
-
-
+	APPLICANT::totalCount();
 	return 0;
 }
+
+
