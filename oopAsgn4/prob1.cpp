100f3d48468b715cc425849b6bd5a79cdbbf012c
debargham14 <debargham14@gmail.com>
2020-12-26 01:42:20 +0530

    assignments added

--- oopAsgn4/prob1.cpp
+++ oopAsgn4/prob1.cpp
@@ -1,4 +1,4 @@
-//necessary header files 
+//necessary header files
 #include<iostream>
 #define MAX_SIZE 100
 using namespace std;
@@ -7,7 +7,7 @@ class APPLICANT;
 class ListOfApplicants; //fwd declarartions done
 
 //designing the class withdraw
-class WITHDRAW{
+class WITHDRAW {
 	int accNo;
 	int amount;
 
@@ -20,7 +20,7 @@ public:
 };
 
 //designing the class applicant
-class APPLICANT{
+class APPLICANT {
 	int accNo;
 	int balance;
 public:
@@ -52,7 +52,7 @@ APPLICANT dummy; // to return incase if function reaches end without returning a
 
 //defining the method update balance
 void WITHDRAW::updateBalance(APPLICANT& obj) {
-	if(obj.balance > amount) { // if the amount to be withdrawn is present
+	if (obj.balance > amount) { // if the amount to be withdrawn is present
 		//perform the relevant operation
 		obj.balance -= amount;
 		cout << "Withdraw Successful !\n";
@@ -64,53 +64,57 @@ void WITHDRAW::updateBalance(APPLICANT& obj) {
 }
 
 //designing the class applicant list to maintain a list of applicants
-class ListOfApplicants{
+class ListOfApplicants {
 	int count = -1;
 	APPLICANT arr[MAX_SIZE];//creating an array of appli
 public:
 	//method to add a new applicant
 	void addApplicant() {
+		if (count == MAX_SIZE) {
+			cout << "Overflow !";
+			return;
+		}
 		int flag = 0;
 		APPLICANT obj;
 		obj.getData();
-		for(int i = 0; i <= count; i++) { //check the uniqueness of the account
-			if(arr[i].accNo == obj.accNo){ //if account already exists
+		for (int i = 0; i <= count; i++) { //check the uniqueness of the account
+			if (arr[i].accNo == obj.accNo) { //if account already exists
 				cout << "Account already exists !\n";
 				flag = 1;
 			}
 		}
-		if(!flag) { //if account does not exists add the account
+		if (!flag) { //if account does not exists add the account
 			arr[++count] = obj;
 		}
 	}
 
-	//method to display an applicant by account number if existing  
+	//method to display an applicant by account number if existing
 	void displayByAccNo(int val) {
 		int flag = 0;
-		for(int i = 0; i <= count; i++) {	//iterating over all the accounts
-			if(arr[i].accNo == val) {
+		for (int i = 0; i <= count; i++) {	//iterating over all the accounts
+			if (arr[i].accNo == val) {
 				cout << "Account Found ! Here are the details :- \n";
 				arr[i].showData();
 				flag = 1;
 			}
 		}
-		if(flag == 0)
+		if (flag == 0)
 			cout << "Account Does Not exists !\n";
-	} 
+	}
 
-	//method to search by acc number 
-	bool searchByAccNo(int val){ //return true/flase depending upon whether account exists or not
-		for(int i = 0; i <= count; i++) {
-			if(arr[i].accNo == val)
+	//method to search by acc number
+	bool searchByAccNo(int val) { //return true/flase depending upon whether account exists or not
+		for (int i = 0; i <= count; i++) {
+			if (arr[i].accNo == val)
 				return 1;
 		}
 		return 0;
-	} 
+	}
 
-	// returning the account (object reference) that matches a particular account number  
+	// returning the account (object reference) that matches a particular account number
 	APPLICANT& retObject(int val) {
-		for(int i = 0; i <= count; i++) {	
-			if(arr[i].accNo == val)
+		for (int i = 0; i <= count; i++) {
+			if (arr[i].accNo == val)
 				return arr[i];
 		}
 		return dummy; //returning dummy object
@@ -119,7 +123,7 @@ public:
 	//method to display all the accounts
 	void displayAll() {
 		cout << "Details Of All Acounts are ------- \n";
-		for(int i = 0; i <= count; i++){
+		for (int i = 0; i <= count; i++) {
 			arr[i].showData();
 			cout << "---------------------------------------\n";
 		}
@@ -136,40 +140,40 @@ void WITHDRAW::getData(APPLICANT& obj) {
 //driver code to implment the same
 int main() {
 	ListOfApplicants obj;
-	while(1) {
+	while (1) {
 		//menu driven code to implment the functionalities
 		cout << "1. add Acount\n2. display account\n3. display All accounts\n4. withdraw\n5. eixt\n";
 		int ch;
-		cin >> ch;	//receiving the user choice 
+		cin >> ch;	//receiving the user choice
 
-		switch(ch) {
-			case 1:
-				obj.addApplicant(); // to add an applicant 
-				break;
-			case 2:
-				int accountNo;
-				cout << "Enter the account number :- ";
-				cin >> accountNo;
-				obj.displayByAccNo(accountNo);	// to diplay an account by the given account number
-				break;
-			case 3:
-				obj.displayAll();	// to diplay all the accounts
-				break;
-			case 4:
-				cout << "Enter the account Number :- ";
-				cin >> accountNo;	// to withdraw from particular account
-				if(obj.searchByAccNo(accountNo)){ //if account exists then withdraw from the account 
-					WITHDRAW w; //declaring withou object
-					w.getData(obj.retObject(accountNo));
-					w.updateBalance(obj.retObject(accountNo)); //call of the update Balance by the withdraw object
-				}
-				else {	// if account does not exist 
-					cout << "Account Doesn't exist !\n";
-				}
-				break;
-			case 5:
-				exit(0); 
-				//exit out of the system
+		switch (ch) {
+		case 1:
+			obj.addApplicant(); // to add an applicant
+			break;
+		case 2:
+			int accountNo;
+			cout << "Enter the account number :- ";
+			cin >> accountNo;
+			obj.displayByAccNo(accountNo);	// to diplay an account by the given account number
+			break;
+		case 3:
+			obj.displayAll();	// to diplay all the accounts
+			break;
+		case 4:
+			cout << "Enter the account Number :- ";
+			cin >> accountNo;	// to withdraw from particular account
+			if (obj.searchByAccNo(accountNo)) { //if account exists then withdraw from the account
+				WITHDRAW w; //declaring withou object
+				w.getData(obj.retObject(accountNo));
+				w.updateBalance(obj.retObject(accountNo)); //call of the update Balance by the withdraw object
+			}
+			else {	// if account does not exist
+				cout << "Account Doesn't exist !\n";
+			}
+			break;
+		case 5:
+			exit(0);
+			//exit out of the system
 		}
 	}
 	return 0;
